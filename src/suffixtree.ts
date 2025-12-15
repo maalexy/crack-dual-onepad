31b617cedd46cdff3fca888a977dfe5ea630ae7d
Alexy Marcell <alexy.marcell@hotmail.com>
2025-12-15 07:35:17 +0100

    added suffix tree

---
+++ src/suffixtree.ts
@@ -0,0 +1,51 @@
+import { EnglishChar, isEnglishChar } from "./charconverter.ts";
+
+type SuffixIndex = string;
+type SuffixNode = Map<EnglishChar, SuffixIndex>; // undefined means impossible step
+
+export class SuffixTree {
+    private data: Map<SuffixIndex, SuffixNode>;
+
+    constructor() {
+        this.data = new Map();
+    }
+
+    getOrNew(si: SuffixIndex) : SuffixNode {
+        if(!this.data.has(si)) {
+            this.data.set(si, new Map<EnglishChar, SuffixIndex>());
+        }
+        return this.data.get(si)!;
+    }
+
+    addWord(word: string) {
+        for(let ix = 0; ix < word.length; ix++) {
+            const prev = word.substring(0, ix);
+            const subword = word.substring(0, ix+1);
+            const char = word[ix]
+            if(isEnglishChar(char)) {
+                this.getOrNew(prev).set(char, subword);
+            } else {
+                throw Error(`Not an english word: ${word}`)
+            }
+        }
+        this.getOrNew(word).set(' ', '');
+    }
+
+    addWordlist(wordlist: string[]) {
+        for(const word of wordlist) {
+            this.addWord(word);
+        }
+    }
+
+    getSafe(si: SuffixIndex) : SuffixNode {
+        if(!this.data.has(si)) {
+            throw Error("Suffix index not found");
+        }
+        return this.data.get(si)!;
+    }
+
+    nexts(si: SuffixIndex) {
+        return this.getSafe(si).entries();
+    }
+}
+
